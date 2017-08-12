1f0c5b864b553c708ecbe002fb33babd0deb4510
Ruddy <ruddy@Daniels-MacBook-Pro.local>
2017-08-12 17:03:42 +0100

    Added missing UglifyJsPlugin config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -23,7 +23,17 @@ module.exports = {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV),
             },
         }),
-        new UglifyJsWebpackPlugin()
+        new UglifyJsWebpackPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        }),
     ],
     output: {
         path: path.join(__dirname, '../dist'),
